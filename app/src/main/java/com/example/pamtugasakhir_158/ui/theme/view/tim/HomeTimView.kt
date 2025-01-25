7b3ddfd53cdb8decfd470293d71d1a8d282f1f1e
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-25 21:09:51 +0700

    Menambahkan HomeViewTim dengan navigasi dan integrasi ViewModel
    Signed-off-by: iqbaldf7 <Iqbaldathonah10@gmail.com>

--- app/src/main/java/com/example/pamtugasakhir_158/ui/theme/view/tim/HomeTimView.kt
+++ app/src/main/java/com/example/pamtugasakhir_158/ui/theme/view/tim/HomeTimView.kt
@@ -1,5 +1,6 @@
 package com.example.pamtugasakhir_158.ui.theme.view.tim
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
