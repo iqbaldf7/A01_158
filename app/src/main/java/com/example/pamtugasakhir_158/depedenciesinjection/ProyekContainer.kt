4083d3fcd292de932b256ad04400dbacac8fac72
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-26 10:09:15 +0700

    menambahkan anggotaRepository
    Signed-off-by: iqbaldf7 <Iqbaldathonah10@gmail.com>

--- app/src/main/java/com/example/pamtugasakhir_158/depedenciesinjection/ProyekContainer.kt
+++ app/src/main/java/com/example/pamtugasakhir_158/depedenciesinjection/ProyekContainer.kt
@@ -1,7 +1,10 @@
 package com.example.pamtugasakhir_158.depedenciesinjection
 
+import com.example.pamtugasakhir_158.repository.AnggotaRepository
+import com.example.pamtugasakhir_158.repository.NetworkAnggotaRepository
 import com.example.pamtugasakhir_158.repository.NetworkTimRepository
 import com.example.pamtugasakhir_158.repository.TimRepository
+import com.example.pamtugasakhir_158.service.AnggotaService
 import com.example.pamtugasakhir_158.service.TimService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -10,7 +13,9 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val timRepository: TimRepository
+    val anggotaRepository: AnggotaRepository
 }
+
 class ProyekContainer : AppContainer {
 
     private val baseUrl = "http://10.0.2.2:4000/api/mahasiswa/"
@@ -19,12 +24,20 @@ class ProyekContainer : AppContainer {
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
-    // Implementasi timRepository
 
+    // Implementasi timRepository
     private val timService: TimService by lazy {
         retrofit.create(TimService::class.java)
     }
     override val timRepository: TimRepository by lazy {
         NetworkTimRepository(timService)
+    }
+
+    // Implementasi anggotaRepository
+    private val anggotaService: AnggotaService by lazy {
+        retrofit.create(AnggotaService::class.java)
+    }
+    override val anggotaRepository: AnggotaRepository by lazy {
+        NetworkAnggotaRepository(anggotaService)
     }
 }
