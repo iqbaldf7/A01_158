12518e1d75543519c0d5562761b004d08387d0ba
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-27 10:12:02 +0700

    melakukan perbaikan dan penyesuaian di TimRepository
    Signed-off-by: iqbaldf7 <Iqbaldathonah10@gmail.com>

--- app/src/main/java/com/example/pamtugasakhir_158/repository/TimRepository.kt
+++ app/src/main/java/com/example/pamtugasakhir_158/repository/TimRepository.kt
@@ -1,44 +1,44 @@
 package com.example.pamtugasakhir_158.repository
-
+import com.example.pamtugasakhir_158.model.AllTimResponse
 import com.example.pamtugasakhir_158.model.Tim
 import com.example.pamtugasakhir_158.service.TimService
 
 
 interface TimRepository {
 
-    suspend fun getAllTim(): List<Tim>
+    suspend fun getAllTim(): AllTimResponse
 
-    suspend fun getTimByID(id_tim: String): Tim
+    suspend fun getTimByID(idTim: String): Tim
 
     suspend fun insertTim(tim: Tim)
 
-    suspend fun updateTim(id_tim: String, tim: Tim)
+    suspend fun updateTim(idTim: String, tim: Tim)
 
-    suspend fun deleteTim(id_tim: String)
+    suspend fun deleteTim(idTim: String)
 }
 
 class NetworkTimRepository(
     private val timApiService: TimService
 ) : TimRepository {
 
-    override suspend fun getAllTim(): List<Tim> =
-        timApiService.getAllTim()
+    override suspend fun getAllTim(): AllTimResponse =
+        timApiService.getTim()
 
-    override suspend fun getTimByID(id_tim: String): Tim {
-        return timApiService.getTimByID(id_tim)
+    override suspend fun getTimByID(idTim: String): Tim {
+        return timApiService.getTimByID(idTim).data
     }
 
     override suspend fun insertTim(tim: Tim) {
         timApiService.insertTim(tim)
     }
 
-    override suspend fun updateTim(id_tim: String, tim: Tim) {
-        timApiService.updateTim(id_tim, tim)
+    override suspend fun updateTim(idTim: String, tim: Tim) {
+        timApiService.updateTim(idTim, tim)
     }
 
-    override suspend fun deleteTim(id_tim: String) {
+    override suspend fun deleteTim(idTim: String) {
         try {
-            val response = timApiService.deleteTim(id_tim)
+            val response = timApiService.deleteTim(idTim)
             if (!response.isSuccessful) {
                 throw Exception("Failed to delete tim. HTTP Status code: ${response.code()}")
             } else {
