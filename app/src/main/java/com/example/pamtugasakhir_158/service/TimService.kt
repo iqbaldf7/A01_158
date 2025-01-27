ca5388c4027a9b876a8870bb9bc796aab8be8faa
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-27 10:12:39 +0700

    perbaikan TimService
    Signed-off-by: iqbaldf7 <Iqbaldathonah10@gmail.com>

--- app/src/main/java/com/example/pamtugasakhir_158/service/TimService.kt
+++ app/src/main/java/com/example/pamtugasakhir_158/service/TimService.kt
@@ -1,35 +1,34 @@
 package com.example.pamtugasakhir_158.service
 
 
+import com.example.pamtugasakhir_158.model.AllTimResponse
 import com.example.pamtugasakhir_158.model.Tim
+import com.example.pamtugasakhir_158.model.TimDetailResponse
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
-import retrofit2.http.Query
-
+import retrofit2.http.Path
 interface TimService {
 
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json",
     )
+    @GET("tim/.")
+    suspend fun getTim(): AllTimResponse
 
-    @GET("bacatim.php")
-    suspend fun getAllTim(): List<Tim>
+    @GET("tim/{idtim}") // Pastikan rute untuk ID juga sesuai
+    suspend fun getTimByID(@Path("idtim") idTim: String): TimDetailResponse
 
-    @GET("baca1tim.php/{id_tim}")
-    suspend fun getTimByID(@Query("id_tim") id_tim: String): Tim
-
-    @POST("inserttim.php")
+    @POST("tim/tim") // Rute untuk menambahkan tim
     suspend fun insertTim(@Body tim: Tim)
-    @PUT("edittim.php/{id_tim}")
-    suspend fun updateTim(@Query("id_tim") id_tim: String, @Body tim: Tim)
 
-    @DELETE("deletetim.php/{id_tim}")
-    suspend fun deleteTim(@Query("id_tim") id_tim: String): retrofit2.Response<Void>
+    @PUT("tim/{id_tim}") // Rute untuk mengedit tim
+    suspend fun updateTim(@Path("id_tim") idTim: String, @Body tim: Tim)
+
+    @DELETE("tim/{id_tim}") // Rute untuk menghapus tim
+    suspend fun deleteTim(@Path("id_tim") idTim: String): retrofit2.Response<Void>
 }
-
-
