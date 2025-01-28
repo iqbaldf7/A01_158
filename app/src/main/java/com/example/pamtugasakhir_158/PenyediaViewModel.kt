f5f15a9a28394f10bd9cc071f65492e060091e47
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-28 13:13:05 +0700

    menambahkan InsertProyekViewModel, HomeProyekViewModel, UpdateProyekViewModel,DetailProyekViewModel Ke PenyediaViewModel
    Signed-off-by: iqbaldf7 <Iqbaldathonah10@gmail.com>

--- app/src/main/java/com/example/pamtugasakhir_158/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pamtugasakhir_158/PenyediaViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pamtugasakhir_158.ui.theme.viewmodel.proyek.DetailProyekViewModel
+import com.example.pamtugasakhir_158.ui.theme.viewmodel.proyek.HomeProyekViewModel
+import com.example.pamtugasakhir_158.ui.theme.viewmodel.proyek.InsertProyekViewModel
+import com.example.pamtugasakhir_158.ui.theme.viewmodel.proyek.UpdateProyekViewModel
 import com.example.pamtugasakhir_158.ui.theme.viewmodel.tim.DetailTimViewModel
 import com.example.pamtugasakhir_158.ui.theme.viewmodel.tim.HomeTimViewModel
 import com.example.pamtugasakhir_158.ui.theme.viewmodel.tim.InsertTimViewModel
@@ -16,6 +20,10 @@ object PenyediaViewModel {
         initializer { InsertTimViewModel(ProyekApplications().container.timRepository) }
         initializer { DetailTimViewModel(ProyekApplications().container.timRepository) }
         initializer { UpdateTimViewModel(createSavedStateHandle(),ProyekApplications().container.timRepository) }
+        initializer { HomeProyekViewModel(ProyekApplications().container.proyekRepository) }
+        initializer { InsertProyekViewModel(ProyekApplications().container.proyekRepository) }
+        initializer { DetailProyekViewModel(ProyekApplications().container.proyekRepository) }
+        initializer { UpdateProyekViewModel(createSavedStateHandle(),ProyekApplications().container.proyekRepository) }
 
     }
 }
